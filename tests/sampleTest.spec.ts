0c26d5678980e454a53fdd28c0faf5bf3623bf32
Taras Dubkovych <tesic1234@gmail.com>
2024-12-02 15:02:25 +0200

    added refactored changes

--- tests/sampleTest.spec.ts
+++ tests/sampleTest.spec.ts
@@ -2,13 +2,22 @@ import {test, expect, selectors} from '@playwright/test'
 import { timeLog } from 'console';
 
 test ('My First Test', async({page})=>{
-    await page.goto('https://www.saucedemo.com/');
-     const title = await page.locator('.login_logo')
+    
+
+    await test.step(`Open base url`, async () => {
+        await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step(`Verify the page title`, async () => {
+        const title = await page.locator('.login_logo')
      console.log(await title.textContent())
     await expect(title).toHaveText('Swag Labs')
+    });
+    
 });
 
-test ('My Second Test', async({page})=>{
+test ('My Second Test withot steps', async({page})=>{
+
     await page.goto('https://www.saucedemo.com/');
     const userNameField = page.locator('#user-name');
     await userNameField.fill("standard_user");
@@ -23,3 +32,31 @@ test ('My Second Test', async({page})=>{
     const expectedText = "Products";
     await expect(page.locator(`//span[contains(text(),'${expectedText}')]`)).toHaveText('Products')
 })
+
+test ('My Second Test with steps', async({page})=>{
+    
+    await test.step(`Open base url`, async () => {
+        await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step(`Fill in username`, async () => {
+        const userNameField = page.locator('#user-name');
+        await userNameField.fill("standard_user");
+    });
+
+    await test.step(`Fill in password`, async () => {
+        const userPassField = page.locator('#password');
+        await userPassField.fill('secret_sauce');
+    });
+
+    await test.step(`Click log in button`, async () => {
+        const loginButton = page.locator('#login-button');
+        await loginButton.click();
+    });
+    
+    await test.step(`Verify text`, async () => {
+        const expectedText = "Products";
+    await expect(page.locator(`//span[contains(text(),'${expectedText}')]`)).toHaveText('Products')
+    });
+   
+});
